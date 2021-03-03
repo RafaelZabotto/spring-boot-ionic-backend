ad0fd6937b48d88f3e757afbef33b32f09044ab1
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2021-03-02 23:44:41 -0300

    mockemailservice with logger and Template Method

--- src/main/java/com/rafael/cursoMC/config/TestConfig.java
+++ src/main/java/com/rafael/cursoMC/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.rafael.cursoMC.config;
 
 import com.rafael.cursoMC.services.DBService;
+import com.rafael.cursoMC.services.EmailService;
+import com.rafael.cursoMC.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +21,10 @@ public class TestConfig {
     public boolean instantiateDatabase() throws ParseException {
         dbService.instantiateTestDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService() {
+        return new MockEmailService();
     }
 }
