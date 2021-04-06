8b5f829a509c86a82dd9dbe3f422c6051eeeed3e
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2021-04-05 22:46:37 -0300

    Header exposure and more size to upload

---
+++ src/main/java/com/rafael/cursoMC/filters/HeaderExposureFilter.java
@@ -0,0 +1,26 @@
+package com.rafael.cursoMC.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HeaderExposureFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.addHeader("acess-control-expose-headers", "location");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
