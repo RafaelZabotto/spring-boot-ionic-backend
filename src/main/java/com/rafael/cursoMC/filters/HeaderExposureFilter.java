c5114399c6aca2b926d6e1cd6d8d0e5afadb4333
Rafael Zabotto <rafaelm.zabotto@gmail.com>
2021-08-02 23:29:45 -0300

    fix HeaderExposureFilter

--- src/main/java/com/rafael/cursoMC/filters/HeaderExposureFilter.java
+++ src/main/java/com/rafael/cursoMC/filters/HeaderExposureFilter.java
@@ -1,9 +1,12 @@
 package com.rafael.cursoMC.filters;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class HeaderExposureFilter implements Filter {
 
     @Override
@@ -15,7 +18,7 @@ public class HeaderExposureFilter implements Filter {
             throws IOException, ServletException {
 
         HttpServletResponse res = (HttpServletResponse) servletResponse;
-        res.addHeader("acess-control-expose-headers", "location");
+        res.addHeader("access-control-expose-headers", "location");
         filterChain.doFilter(servletRequest, servletResponse);
 
     }
